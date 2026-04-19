a48f13d7b351237eed4cfc3db7da79570e2e8382
Mihary-Mandresy <miharymandresy12@gmail.com>
2026-04-19 22:13:21 +0300

    feat: fonction save avec test

--- demo/src/main/java/com/visa/demo/models/Demande.java
+++ demo/src/main/java/com/visa/demo/models/Demande.java
@@ -23,6 +23,93 @@ public class Demande extends Entity {
         setSigle("DMD");
     }
 
+    /**
+     * 
+     * @param c
+     * @param etatCivil
+     * @param passport
+     * @param visa
+     * @return
+     * @throws Exception
+     */
+    public Demande save(Connection c, Demandeur demandeur, Passport passport, Visatransformable visa,
+            List<String> dossiersStandard, List<String> dossiersSup, String idTypeDemande, String idTypeVisa,
+            LocalDate date)
+            throws Exception {
+        try {
+            c.setAutoCommit(false);
+
+            // Insertion Detaitl
+            demandeur.insert(c);
+
+            passport.setIddemandeur(demandeur.getId());
+            passport.insert(c);
+
+            visa.setIddemandeur(demandeur.getId());
+            visa.setIdpassport(passport.getId());
+            visa.insert(c);
+
+            // Insertion Demande
+            Demande demande = new Demande();
+            demande.setIddemandeur(demandeur.getId());
+            demande.setIdpassport(passport.getId());
+            demande.setIdvisatransformable(visa.getId());
+            demande.setDatecreation(date);
+            demande.setIdetatdemande("ETATDMD000001");
+
+            // DANGER mila ovaina
+            demande.setIdtypedemande(idTypeDemande);
+            demande.setIdtypevisa(idTypeVisa);
+
+            demande.insert(c);
+
+            // Insertion dossier fournit
+
+            for (String idStandard : dossiersStandard) {
+                CheckDossierStandard checkdossier = new CheckDossierStandard();
+                checkdossier.setIddemande(demande.getId());
+                checkdossier.setIddossierstandard(idStandard);
+                checkdossier.setExist(true);
+
+                checkdossier.insert(c);
+            }
+
+            for (String idsupplementaire : dossiersSup) {
+                CheckDossierSupplementaire checkdossier = new CheckDossierSupplementaire();
+                checkdossier.setIddemande(demande.getId());
+                checkdossier.setIddossiersupplementaire(idsupplementaire);
+                checkdossier.setExist(true);
+
+                checkdossier.insert(c);
+            }
+
+            c.commit();
+
+            c.setAutoCommit(true);
+
+            return demande;
+        } catch (Exception e) {
+            c.rollback();
+            throw e;
+        }
+    }
+
+    // =========== Verification (Mbola tsy vita tsara ) ===========
+
+    public boolean isFullStandard(Connection c, List<String> idStandard) throws Exception {
+        return new DossierStandard().findAll(c).stream()
+                .map(e -> (DossierStandard) e)
+                .allMatch(e -> idStandard.contains(e.getId()));
+    }
+
+    public boolean isFullSup(Connection c, List<String> idSup, String idStatut) throws Exception {
+        return new DossierSupplementaire().select(c, String.format("idstatutvisa like \"%s\"", idStatut), null).stream()
+                .map(e -> (DossierSupplementaire) e)
+                .allMatch(e -> idSup.contains(e.getId()));
+    }
+
+    // =========== Verification ===========
+
     public String getIdtypedemande() {
         return idtypedemande;
     }
